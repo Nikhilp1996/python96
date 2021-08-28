00326fc62930806a221f56c7b6e92525292775c5
Nikhil P <89653032+Nikhilp1996@users.noreply.github.com>
2021-08-28 14:07:52 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,18 +1,5 @@
-i = 2,3
->>> print(i+i)
-(2, 3, 2, 3)
+ a = 2
+>>> b = 8
 >>> 
->>> 2
-2
->>> 3
-3
->>> 2+3 = 5
-SyntaxError: cannot assign to operator
->>> 2+3
-5
->>> 2
-2
->>> 3
-3
->>> 2+3
-5
+>>> print(a+b)
+10
