0a8186d2445ebf569a870bc0043393d0d4187411
Nikhil P <89653032+Nikhilp1996@users.noreply.github.com>
2021-08-28 13:40:57 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,3 +1,12 @@
 A = "HELLO I AM NIKHIL"
 >>> print (A*5)
 HELLO I AM NIKHILHELLO I AM NIKHILHELLO I AM NIKHILHELLO I AM NIKHILHELLO I AM NIKHIL
+
+for i in range(5):
+    print ("Hello everyone i am Nikhil")
+
+Hello everyone i am Nikhil
+Hello everyone i am Nikhil
+Hello everyone i am Nikhil
+Hello everyone i am Nikhil
+Hello everyone i am Nikhil
